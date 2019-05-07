0a52fd645609f5aa06ad72e1de8dcafa17ed5371
Daniel Pavlov <danieldpavlovv@gmail.com>
2019-05-08 02:03:51 +0300

    zadNo-2- str.28/zad.12

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {//str.28/zad.12
+        public static bool CheckIfPolidrom(string word)
+        {
+            
+            char[] charWord = word.ToCharArray();
+            int reverseCount = word.Length - 1;
+            bool isTrue = true;
+            for (int i = 0; i < charWord.Length; i++)
+            {
+                if (charWord[i] != charWord[reverseCount]) isTrue = false;
+                reverseCount--;
+            }
+            return isTrue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check if word is polidrom");
+            Console.WriteLine(CheckIfPolidrom(Console.ReadLine()));
+        }
+    }
+}
