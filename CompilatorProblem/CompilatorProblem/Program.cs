0ef82f6d3bb5f986d3fb66d462145de831b0b785
Daniel Pavlov <danieldpavlovv@gmail.com>
2019-05-08 02:07:57 +0300

    zadNo-3- str.61/zad.1

---
+++ CompilatorProblem/CompilatorProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilatorProblem//str61/zad1
+{
+    class Program
+    {
+       
+        static bool isParenthCorrect(string parenths)
+        {
+
+            int openCount = 0;//opening parenths
+
+            if (parenths.Length <= 1)
+                return false;
+
+            for (int i = 0; i < parenths.Length; i++)
+            {
+                if (parenths[i].Equals('('))
+                    openCount++;
+
+                else if (parenths[i].Equals(')'))
+                {
+                    openCount--;//pops opening parenths whenever a closing is found!
+                    if (openCount < 0)
+                        return false;
+                }
+            }
+
+            return (openCount == 0);
+        }
+        static void Main(string[] args)
+        {
+            //testing 
+            string test1 = "((())())()";//should return true;
+            string test2 = ")()(";//should return false;
+            string test3 = "())";//should return false
+            Console.WriteLine(test1 + " returns " + isParenthCorrect(test1));
+            Console.WriteLine(test2 + " returns " + isParenthCorrect(test2));
+            Console.WriteLine(test3 + " returns " + isParenthCorrect(test3));
+
+            
+        }
+    }
+}
